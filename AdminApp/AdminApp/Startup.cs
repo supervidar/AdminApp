0e576596997679f41a6e7e389cdff393f463eb57
Vidar Heltne <helt007@hotmail.com>
2017-01-19 10:36:08 +0100

    Endring i prosjekt type.

---
+++ AdminApp/AdminApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdminApp.Startup))]
+namespace AdminApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
